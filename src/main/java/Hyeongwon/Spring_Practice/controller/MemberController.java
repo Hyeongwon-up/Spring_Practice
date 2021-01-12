25dc80e92e2482e4637b36a8f9e5a44d77cbd7b7
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-12 18:39:13 +0900

    210112

--- src/main/java/Hyeongwon/Spring_Practice/controller/MemberController.java
+++ src/main/java/Hyeongwon/Spring_Practice/controller/MemberController.java
@@ -1,9 +1,12 @@
 package Hyeongwon.Spring_Practice.controller;
 
 
+import Hyeongwon.Spring_Practice.domain.Member;
 import Hyeongwon.Spring_Practice.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -13,6 +16,23 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+
+    @GetMapping("/members/new")
+    public String createFrom() {
+        return "members/createMemberForm";
+    }
+
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 
 }
