80c33ac40cfc3a3cd6987a8be3b4dabec696ac16
Hyeongwon-up <leehwdatabase@gmail.com>
2021-01-13 06:13:18 +0900

    JDBC

--- src/main/java/Hyeongwon/Spring_Practice/SpringConfig.java
+++ src/main/java/Hyeongwon/Spring_Practice/SpringConfig.java
@@ -1,16 +1,27 @@
 package Hyeongwon.Spring_Practice;
 
 
+import Hyeongwon.Spring_Practice.repository.JdbcMemberRepository;
 import Hyeongwon.Spring_Practice.repository.MemberRepository;
 import Hyeongwon.Spring_Practice.repository.MemoryMemberRepsitory;
 import Hyeongwon.Spring_Practice.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -19,7 +30,10 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepsitory();
+
+
+        return new JdbcMemberRepository(dataSource);
+        //return new MemoryMemberRepsitory();
     }
 
 }
